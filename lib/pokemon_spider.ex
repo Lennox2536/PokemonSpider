2af1c7207f2cfc3b91dfebff6bd1ffcf4edeed22
Lennox2536 <jakub.gorecki19@gmail.com>
2023-10-04 20:34:16 +0200

    Add an argument that limits the number of pages requested

--- lib/pokemon_spider.ex
+++ lib/pokemon_spider.ex
@@ -9,18 +9,24 @@ defmodule PokemonSpider do
     :world
   end
 
-  def get_pokemons() do
-    page_number = get_page_number()
+  def get_pokemons(page_limit \\ -1) do
+    page_number =
+      get_page_number()
+      |> apply_limit(page_limit)
 
-    pokemon_lists = for page_number <- 1..page_number, into: [] do
-      @base_url
-      |> append_page_number(page_number)
-      |> Request.call()
-      |> PokemonPageSpider.pokemons()
-    end
+    pokemon_lists =
+      for page_number <- 1..page_number, into: [] do
+        @base_url
+        |> append_page_number(page_number)
+        |> Request.call()
+        |> PokemonPageSpider.pokemons()
+      end
 
-    File.write!("output/result.json", Poison.encode!(List.flatten(pokemon_lists)), [:binary])
+    File.write!("result.json", Poison.encode!(List.flatten(pokemon_lists)), [:binary])
   end
+
+  defp apply_limit(_page_number, limit) when limit > 0, do: limit
+  defp apply_limit(page_number, _limit), do: page_number
 
   defp get_page_number() do
     Request.call(@base_url)
